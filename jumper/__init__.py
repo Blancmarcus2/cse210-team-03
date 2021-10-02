1f29810fd2737e09d148fb6056db130b88d1a551
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2021-10-02 11:32:38 +0200

    Update __init__.py

--- jumper/__init__.py
+++ jumper/__init__.py
@@ -1,20 +1,5 @@
 """
 The root package contains all the source code for the game.
 """
-import pygame
-
-# constants
-
-# Colors
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-
-
-# Screen dimensions view
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 
