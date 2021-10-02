bef9765d7b127fc03c1cf69685243c3710e35b6a
owamamwen1 <90800458+owamamwen1@users.noreply.github.com>
2021-10-02 11:34:01 +0200

    Update __init__.py

--- jumper/game/__init__.py
+++ jumper/game/__init__.py
@@ -2,6 +2,8 @@
 The game package contains specific classes for playing Jumper.
 """
 import pygame
-class Player(pygame.sprite.Sprite):
-# write code here
+
+def start_game():
+    class Player(pygame.sprite.Sprite):
+    # write code here
  
